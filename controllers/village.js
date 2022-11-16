d27d4752d81a4fbae32137bdc11956d28795f089
PratapKumarChowdary <S555080@nwmissouri.edu>
2022-11-16 14:36:38 -0600

    Added  create page for villages

--- controllers/village.js
+++ controllers/village.js
@@ -127,3 +127,17 @@ exports.village_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.village_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('villagecreate', { title: 'Village Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/village.js
+++ routes/village.js
@@ -6,5 +6,7 @@ var router = express.Router();
 router.get('/', village_controlers.village_view_all_Page ); 
 /* GET detail costume page */ 
 router.get('/detail', village_controlers.village_view_one_Page); 
+/* GET create costume page */ 
+router.get('/create', village_controlers.village_create_Page); 
 module.exports = router; 
 
